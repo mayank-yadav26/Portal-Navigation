6807b6f9a7e93290d076984223d212c92764fc9c
mayank yadav <mayankyadav1503@gmail.com>
2025-03-11 12:00:37 +0530

    Use lombok for model class

--- portal_navigation/src/main/java/com/spring/model/NavigationDetails.java
+++ portal_navigation/src/main/java/com/spring/model/NavigationDetails.java
@@ -1,5 +1,8 @@
 package com.spring.model;
 
+import lombok.Data;
+
+@Data
 public class NavigationDetails {
 	private int navigationId;
 	private String navigationName;
@@ -7,42 +10,4 @@ public class NavigationDetails {
 	private String requestType;
 	private String parameters;
 	private String requestHeaders;
-	
-	public int getNavigationId() {
-		return navigationId;
-	}
-	public void setNavigationId(int navigationId) {
-		this.navigationId = navigationId;
-	}
-	public String getNavigationName() {
-		return navigationName;
-	}
-	public void setNavigationName(String navigationName) {
-		this.navigationName = navigationName;
-	}
-	public String getBaseUrl() {
-		return baseUrl;
-	}
-	public void setBaseUrl(String baseUrl) {
-		this.baseUrl = baseUrl;
-	}
-	public String getRequestType() {
-		return requestType;
-	}
-	public void setRequestType(String requestType) {
-		this.requestType = requestType;
-	}
-	public String getParameters() {
-		return parameters;
-	}
-	public void setParameters(String parameters) {
-		this.parameters = parameters;
-	}
-	public String getRequestHeaders() {
-		return requestHeaders;
-	}
-	public void setRequestHeaders(String requestHeaders) {
-		this.requestHeaders = requestHeaders;
-	}
-	
 }
